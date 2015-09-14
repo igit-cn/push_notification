40524b56e0436f3816984d525f160d566926dc0d
tianzhiyu <tzy313@gmail.com>
2015-09-14 16:59:54 +0800

    add the test ..

--- generator/src/main/java/com/yidian/push/generator/test/Test.java
+++ generator/src/main/java/com/yidian/push/generator/test/Test.java
@@ -1,8 +1,12 @@
 package com.yidian.push.generator.test;
 
 import com.yidian.push.utils.DateUtil;
+import com.yidian.push.utils.GsonFactory;
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -42,6 +46,11 @@ public class Test {
         service.shutdown();
         service.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
         System.out.println("all thread complete");
+        System.out.println(GsonFactory.getPrettyGson().toJson(null));
+        System.out.println(GsonFactory.getPrettyGson().toJson(new HashSet<Integer>()));
+        System.out.println(GsonFactory.getPrettyGson().toJson(new HashSet<>(Arrays.asList(1,2,3,5,6,8,99,1,2,3,4,4))));
+        System.out.println(GsonFactory.getPrettyGson().toJson(new ArrayList<>()));
+        System.out.println(GsonFactory.getPrettyGson().toJson(new ArrayList<>(Arrays.asList(1,2,3,5,6,8,99,1,2,3,4,4))));
 
 
     }
