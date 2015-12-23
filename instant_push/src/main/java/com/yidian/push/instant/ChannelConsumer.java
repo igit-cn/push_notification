bbbe3b75804092992968c3629330cdfb109bdce9
tianzhiyu <tzy313@gmail.com>
2015-12-23 23:39:26 +0800

    kafa can get data ...

--- instant_push/src/main/java/com/yidian/push/instant/ChannelConsumer.java
+++ instant_push/src/main/java/com/yidian/push/instant/ChannelConsumer.java
@@ -1,6 +1,7 @@
 package com.yidian.push.instant;
 
 import com.google.common.collect.ImmutableMap;
+import com.yidian.push.config.Config;
 import com.yidian.push.config.InstantPushConfig;
 import com.yidian.push.utils.GsonFactory;
 import kafka.consumer.ConsumerConfig;
@@ -10,10 +11,12 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.message.MessageAndMetadata;
 
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import kafka.utils.ZkUtils;
 import lombok.extern.log4j.Log4j;
 
 /**
@@ -25,11 +28,11 @@ public class ChannelConsumer {
     private static final String ZOOKEEPER = "zookeeper.connect";
     private InstantPushConfig config = null;
 
-    public ChannelConsumer (InstantPushConfig config) {
+    public ChannelConsumer(InstantPushConfig config) {
         this.config = config;
     }
 
-    public void run2(){
+    public void run2() {
 //        "serializer.class": "kafka.serializer.StringEncoder",
 //                // "compression.codec": "2",
 //                // "compressed.topics": "relatedchannel",
@@ -52,12 +55,11 @@ public class ChannelConsumer {
 
         String groupId = "relatedchannel_group_id";
         properties.put("group.id", groupId);
-        properties.put( "zookeeper.connect", zk);
-        // ZkUtils.maybeDeletePath(zk, "/consumers/" + groupId);
+        properties.put("zookeeper.connect", zk);
+        ZkUtils.maybeDeletePath(zk, "/consumers/" + groupId);
 
         ConsumerConfig consumerConfig = new ConsumerConfig(properties);
         ConsumerConnector consumerConnector = kafka.consumer.Consumer.createJavaConsumerConnector(consumerConfig);
-
         Map<String, Integer> map = new HashMap<>();
         map.put(topicName, consumerNum);
 
@@ -69,32 +71,39 @@ public class ChannelConsumer {
         // create list of # threads to consume from each of the partitions
         ExecutorService executor = Executors.newFixedThreadPool(consumerNum);
 
-        // consume the messages in the threads
-        for(final KafkaStream<byte[], byte[]> stream: streams) {
-//            executor.submit(new Runnable() {
-//                public void run() {
-//                    try {
-//                        for (MessageAndMetadata<byte[], byte[]> msgAndMetadata : stream) {
-//                            // process message (msgAndMetadata.message())
-//                            System.out.println(msgAndMetadata.message());
-//                        }
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
+        try {
+            // consume the messages in the threads
+            for (final KafkaStream<byte[], byte[]> stream : streams) {
+                executor.submit(new Runnable() {
+                    public void run() {
+                        try {
+                            for (MessageAndMetadata<byte[], byte[]> msgAndMetadata : stream) {
+                                // process message (msgAndMetadata.message())
+                                byte[] bytes = msgAndMetadata.message();
+                                System.out.println(bytes);
+                                System.out.println(new String(bytes));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+//                ConsumerIterator<byte[], byte[]> it = stream.iterator();
+//                while (it.hasNext()) {
+//                    MessageAndMetadata<byte[], byte[]> msg = it.next();
+//                    byte[] bytes = msg.message();
+//                    System.out.println(bytes);
+//                    System.out.println(new String(bytes));
 //                }
-//            });
-            ConsumerIterator<byte[], byte[]> it = stream.iterator();
-            while (it.hasNext()) {
-                MessageAndMetadata<byte[], byte[]> msg = it.next();
-                byte[] bytes = msg.message();
-                System.out.println(bytes);
-                System.out.println(new String(bytes));
             }
+        } finally {
+            log.info("shutdown the connector ... ");
+            //consumerConnector.shutdown();
         }
     }
 
 
-    public void run(){
+    public void run() {
         // Create the connection to the cluster
         Properties properties = config.getConsumerProperties();
         String topicName = config.getConsumerTopicName();
@@ -102,7 +111,7 @@ public class ChannelConsumer {
         String zk = config.getConsumerProperties().getProperty(ZOOKEEPER);
         String groupId = config.getConsumerProperties().getProperty(GROUP_ID, config.getConsumerTopicName() + "-" + new Random().nextInt());
         properties.put(GROUP_ID, groupId);
-       // ZkUtils.maybeDeletePath(zk, "/consumers/" + groupId);
+        // ZkUtils.maybeDeletePath(zk, "/consumers/" + groupId);
 
         topicName = "relatedchannel";
         consumerNum = 1;
@@ -110,11 +119,11 @@ public class ChannelConsumer {
 
         groupId = "relatedchannel_group_id";
         properties.put("group.id", groupId);
-        properties.put( "zookeeper.connect", zk);
+        properties.put("zookeeper.connect", zk);
         System.out.println(GsonFactory.getDefaultGson().toJson(properties));
 
         ConsumerConfig consumerConfig = new ConsumerConfig(properties);
-        ConsumerConnector consumerConnector =  kafka.consumer.Consumer.createJavaConsumerConnector(consumerConfig);
+        ConsumerConnector consumerConnector = kafka.consumer.Consumer.createJavaConsumerConnector(consumerConfig);
 
         Map<String, Integer> map = new HashMap<>();
         map.put(topicName, consumerNum);
@@ -127,31 +136,34 @@ public class ChannelConsumer {
         ExecutorService executor = Executors.newFixedThreadPool(consumerNum);
 
         // consume the messages in the threads
-        for(final KafkaStream<byte[], byte[]> stream: streams) {
-//            executor.submit(new Runnable() {
-//                public void run() {
-//                    try {
-//                        for (MessageAndMetadata<byte[], byte[]> msgAndMetadata : stream) {
-//                            // process message (msgAndMetadata.message())
-//                            System.out.println(msgAndMetadata.message());
-//                        }
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-            ConsumerIterator<byte[], byte[]> it = stream.iterator();
-            while (it.hasNext()) {
-                MessageAndMetadata<byte[], byte[]> msg = it.next();
-                byte[] bytes = msg.message();
-                System.out.println(bytes);
-                System.out.println(String.valueOf(bytes));
-            }
+        for (final KafkaStream<byte[], byte[]> stream : streams) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    try {
+                        for (MessageAndMetadata<byte[], byte[]> msgAndMetadata : stream) {
+                            // process message (msgAndMetadata.message())
+                            byte[] bytes = msgAndMetadata.message();
+                            System.out.println(bytes);
+                            System.out.println(new String(bytes));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+//            ConsumerIterator<byte[], byte[]> it = stream.iterator();
+//            while (it.hasNext()) {
+//                MessageAndMetadata<byte[], byte[]> msg = it.next();
+//                byte[] bytes = msg.message();
+//                System.out.println(bytes);
+//                System.out.println(new String(bytes));
+//            }
         }
     }
 
-    public static void main(String[] args) {
-        new ChannelConsumer(null).run2();
+    public static void main(String[] args) throws IOException {
+        Config.setCONFIG_FILE("instant_push/src/main/resources/config/config.json");
+        new ChannelConsumer(Config.getInstance().getInstantPushConfig()).run();
     }
 
 
--- instant_push/src/main/java/com/yidian/push/instant/services/Service.java
+++ instant_push/src/main/java/com/yidian/push/instant/services/Service.java
@@ -1,13 +1,15 @@
 package com.yidian.push.instant.services;
 
-//import com.yidian.push.config.Config;
-//import com.yidian.push.config.InstantPushConfig;
+import com.yidian.push.config.Config;
+import com.yidian.push.config.InstantPushConfig;
 import com.yidian.push.instant.ChannelConsumer;
-//import com.yidian.push.utils.FileLock;
-//import com.yidian.push.utils.GsonFactory;
+
+import com.yidian.push.utils.FileLock;
+import com.yidian.push.utils.GsonFactory;
 import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.io.IOException;
 
@@ -18,83 +20,69 @@ import java.io.IOException;
 public class Service implements Runnable {
     private static volatile boolean keepRunning = false;
 
-//    @Override
-//    public void run() {
-//        InstantPushConfig config = null;
-//        try {
-//            config = Config.getInstance().getInstantPushConfig();
-//            log.info("Logging config is " + GsonFactory.getPrettyGson().toJson(config));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-////        try {
-////            keepRunning = true;
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////            log.error("push notification logging init ...");
-////            throw new RuntimeException(e);
-////        }
-//
-//        final Thread currentThread = Thread.currentThread();
-//        Runtime.getRuntime().addShutdownHook(new Thread() {
-//            @Override
-//            public void run() {
-//                keepRunning = false;
-//                log.info("receive kill signal ...");
-//                try {
-//                    currentThread.join();
-//                    log.info("shutdown the thread pools");
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        int sleepTime = config.getRequestScanIntervalInSeconds() * 1000;
-//
-//        new ChannelConsumer(config).run2();
-////        while(keepRunning) {
-////            try {
-////                LogProducer.process();
-////            } catch (Exception e) {
-////                log.error("process failed with exception:" + ExceptionUtils.getFullStackTrace(e));
-////            }
-////            try {
-////                Thread.sleep(sleepTime);
-////            } catch (InterruptedException e) {
-////                log.error("sleep failed...");
-////            }
-////        }
-//    }
-
-    public static void main(String[] args) throws IOException {
-        Logger.getRootLogger().setLevel(Level.DEBUG);
-        new ChannelConsumer(null).run2();;
-//
-//        System.out.println(args.length);
-//        if (args.length >= 1) {
-//            String configFile = args[0];
-//            System.out.println("User specified config file " + configFile);
-//            Config.setCONFIG_FILE(configFile);
-//        } else {
-//           // Config.setCONFIG_FILE("generator/src/main/resources/config/prod_config.json");
-//            Config.setCONFIG_FILE("instant_push/src/main/resources/config/config.json");
-//            System.setProperty("log4j.configuration", "src/main/resources/config/log4j_debug.properties");
-//            //PropertyConfigurator.configure("instant_push/src/main/resources/config/log4j_debug.properties");
-//            //Logger.getRootLogger().setLevel(Level.DEBUG);
-//        }
-//        String lockFile = Config.getInstance().getInstantPushConfig().getLockFile();
-//        if (!FileLock.lockInstance(lockFile)) {
-//            System.out.println("One instance is already running, just quit.");
-//            System.exit(1);
-//        }
-//        //new ChannelConsumer(null).run2();;
-//        Service service = new Service();
-//        service.run();
-
-    }
-
     @Override
     public void run() {
+        InstantPushConfig config = null;
+        try {
+            config = Config.getInstance().getInstantPushConfig();
+            log.info("Logging config is " + GsonFactory.getPrettyGson().toJson(config));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        try {
+//            keepRunning = true;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            log.error("push notification logging init ...");
+//            throw new RuntimeException(e);
+//        }
+
+        final Thread currentThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                keepRunning = false;
+                log.info("receive kill signal ...");
+                try {
+                    currentThread.join();
+                    log.info("shutdown the thread pools");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        int sleepTime = config.getRequestScanIntervalInSeconds() * 1000;
+
+        new ChannelConsumer(config).run2();
 
     }
+
+    public static void main(String[] args) throws IOException {
+//        Logger.getRootLogger().setLevel(Level.DEBUG);
+//        Config.setCONFIG_FILE("instant_push/src/main/resources/config/config.json");
+
+        //new ChannelConsumer(Config.getInstance().getInstantPushConfig()).run2();;
+
+        System.out.println(args.length);
+        if (args.length >= 1) {
+            String configFile = args[0];
+            System.out.println("User specified config file " + configFile);
+            Config.setCONFIG_FILE(configFile);
+        } else {
+            Config.setCONFIG_FILE("instant_push/src/main/resources/config/config.json");
+            System.setProperty("log4j.configuration", "src/main/resources/config/log4j_debug.properties");
+            PropertyConfigurator.configure("instant_push/src/main/resources/config/log4j_debug.properties");
+            Logger.getRootLogger().setLevel(Level.DEBUG);
+        }
+        String lockFile = Config.getInstance().getInstantPushConfig().getLockFile();
+        if (!FileLock.lockInstance(lockFile)) {
+            System.out.println("One instance is already running, just quit.");
+            System.exit(1);
+        }
+        // new ChannelConsumer(null).run2();;
+        Service service = new Service();
+        service.run();
+
+    }
+
 }
